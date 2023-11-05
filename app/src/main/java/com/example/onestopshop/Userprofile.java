5e8d84747e542d546c69703c66da237e54006087
Anhad928 <94036563+Anhad928@users.noreply.github.com>
2023-11-05 01:00:26 -0600

    commit

---
+++ app/src/main/java/com/example/onestopshop/Userprofile.java
@@ -0,0 +1,47 @@
+package com.example.onestopshop;
+
+public class Userprofile{
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getImageUrl() {
+
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+
+        this.imageUrl = imageUrl;
+    }
+}
