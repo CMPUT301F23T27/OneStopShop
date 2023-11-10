a8ccc7bb2e4d281bcbebf8a79906340ed648a551
Romanshu-Bedi <93629205+Romanshu-Bedi@users.noreply.github.com>
2023-11-10 14:11:01 -0700

    updated java docs and comments

--- app/src/main/java/com/example/onestopshop/AddItemActivity.java
+++ app/src/main/java/com/example/onestopshop/AddItemActivity.java
@@ -3,21 +3,25 @@ package com.example.onestopshop;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
+/**
+ * Activity for adding a new item to the inventory.
+ */
 public class AddItemActivity extends AppCompatActivity {
+
+    // Inventory controller to manage inventory data
     private InventoryController inventoryController;
+
+    // UI elements
     private EditText itemNameText;
     private EditText descriptionText;
     private EditText purchaseDateText;
@@ -31,6 +35,7 @@ public class AddItemActivity extends AppCompatActivity {
     private Button confirm;
     private ImageView scanButton;
 
+    // Activity result launcher for handling results from ScanActivity
     private final ActivityResultLauncher<Intent> startForResult =
             registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                     result -> {
@@ -49,7 +54,10 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        // Initialize the 'Confirm' button
         confirm = findViewById(R.id.btn_add_item);
+
         // Initialize Views
         itemNameText = findViewById(R.id.itemName);
         descriptionText = findViewById(R.id.description);
@@ -62,6 +70,8 @@ public class AddItemActivity extends AppCompatActivity {
         commentsText = findViewById(R.id.comments);
         btnCancel = findViewById(R.id.btnCancel);
         scanButton = findViewById(R.id.scanButton);
+
+        // Set up click listener for the 'Scan' button
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,9 +80,12 @@ public class AddItemActivity extends AppCompatActivity {
                 startForResult.launch(intent);
             }
         });
+
+        // Set up click listener for the 'Confirm' button
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Extract data from UI elements
                 String itemName = itemNameText.getText().toString();
                 String description = descriptionText.getText().toString();
                 String purchaseDate = purchaseDateText.getText().toString();
@@ -82,8 +95,12 @@ public class AddItemActivity extends AppCompatActivity {
                 Double estimatedValue = Double.parseDouble(estimatedValueText.getText().toString());
                 List<String> tags = Arrays.asList(tagsText.getText().toString().split(","));
                 String comments = commentsText.getText().toString();
+
+                // Validate the item
                 boolean validItem = validItem();
-                if(validItem){
+
+                // If the item is valid, add it to the inventory
+                if (validItem) {
                     inventoryController = new InventoryController();
                     inventoryController.addItem(new Item(itemName, description, purchaseDate, make,
                             model, estimatedValue, comments, serialNumber, tags));
@@ -91,6 +108,8 @@ public class AddItemActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Set up click listener for the 'Cancel' button
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,7 +117,15 @@ public class AddItemActivity extends AppCompatActivity {
             }
         });
     }
-    public boolean validItem(){
+
+    /**
+     * Validates the item before adding it to the inventory.
+     *
+     * @return True if the item is valid, false otherwise.
+     */
+    public boolean validItem() {
+        // TODO: Add validation logic
         return true;
     }
 }
+
