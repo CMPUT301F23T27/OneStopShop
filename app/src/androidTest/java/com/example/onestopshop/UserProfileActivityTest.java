398d5281582a945e28e304a18aae694e5edf4ebb
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2023-12-03 22:49:30 -0700

    Update UserProfileActivityTest.java

--- app/src/androidTest/java/com/example/onestopshop/UserProfileActivityTest.java
+++ app/src/androidTest/java/com/example/onestopshop/UserProfileActivityTest.java
@@ -1,11 +1,15 @@
 package com.example.onestopshop;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 
@@ -19,27 +23,33 @@ public class UserProfileActivityTest {
     public ActivityScenarioRule<UserProfileActivity> activityRule =
             new ActivityScenarioRule<>(UserProfileActivity.class);
 
+    @Rule
+    public IntentsTestRule<UserProfileActivity> intentsTestRule =
+            new IntentsTestRule<>(UserProfileActivity.class);
+
     @Test
-    public void testEmail(){
-        Espresso.onView(ViewMatchers.withId(R.id.tvEmail))
-                .check(ViewAssertions.matches(withText("Email: vehla996@gmail.com")));
+    public void testEmail() {
+        InventoryController inventoryController = new InventoryController();
+        String email = inventoryController.getUserEmail();
+        onView(withId(R.id.tvEmail))
+                .check(matches(withText("Email: " + email)));
     }
 
     @Test
-    public void testName(){
-        Espresso.onView(ViewMatchers.withId(R.id.tvName))
-                .check(ViewAssertions.matches(withText("Name: Anha")));
+    public void testName() {
+        InventoryController inventoryController = new InventoryController();
+        String name = inventoryController.getUserName();
+        onView(withId(R.id.tvName))
+                .check(matches(withText("Name: " + name)));
     }
 
     @Test
-    public void testlButton() {
-        // Click on the logout button
-        Espresso.onView(ViewMatchers.withId(R.id.btnLogout))
-                .perform(ViewActions.click());
+    public void testLogoutProcess() {
+        // Click the logout button
+        onView(withId(R.id.btnLogout)).perform(click());
 
-        // Verify that LoginActivity is launched after logout
-        Espresso.onView(ViewMatchers.withId(R.id.login))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        // Since we cannot verify the Toast directly, we assume the logout process is successful if the LoginActivity is launched
+        intended(allOf(hasComponent(LoginActivity.class.getName())));
+
     }
-
 }
