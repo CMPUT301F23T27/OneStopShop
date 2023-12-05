42435858e7e0f4ddfede0e665d515e6cb804d03b
AhmadOlagunju <133206365+AhmadOlagunju@users.noreply.github.com>
2023-12-05 12:03:57 -0700

    added tests

--- app/src/androidTest/java/com/example/onestopshop/FilterActivityTest.java
+++ app/src/androidTest/java/com/example/onestopshop/FilterActivityTest.java
@@ -1,7 +1,152 @@
 package com.example.onestopshop;
 
-/*
-    NOTE: YOU MUST LOG IN MANUALLY FIRST BY RUNNING THE APP FOR TEST TO WORK DUE TO AUTH
- */
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.concurrent.CountDownLatch;
+
 public class FilterActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+    private String itemName = "Test";
+    private String description = "this is a test";
+    private String purchaseDate = "2021-01-02";
+    private String make = "apple";
+    private String model = "testModel";
+    private Double estimatedValue = 200.0;
+    private String tag = "device";
+
+    @Test
+    public void testCombinedFilter() {
+        onView(withId(R.id.textViewLogin)).perform(click());
+        addTestItem();
+        purchaseDate = "2021-07-01";
+        tag = "use";
+        addTestItem();
+        purchaseDate = "2022-05-01";
+        itemName = "Mac";
+        addTestItem();
+        /*itemName = "useHeadphones";
+        purchaseDate = "2021-07-04";
+        addTestItem();
+        make = "Sony";
+        itemName = "DontUse";
+        addTestItem();
+        make = "apple";
+        itemName = "useIphone";
+        addTestItem();*/
+        onView(withId(R.id.filter_button)).perform(click());
+        onView(withId(R.id.make)).perform(typeText("apple"), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.tags)).perform(typeText("use"), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.select_button)).perform(click());
+        onView(withId(R.id.item_list)).check(new RecyclerViewItemCountAssertion(2));
+
+        onView(withId(R.id.filter_button)).perform(click());
+
+        onView(withId(R.id.reset_button)).perform(click());
+        deleteItem();
+        deleteItem();
+        deleteItem();
+
+    }
+
+    public void addTestItem() {
+        onView(withId(R.id.add_button)).perform(click());
+        onView(withId(R.id.itemName)).perform(typeText(itemName), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.description)).perform(typeText(description), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.purchaseDate)).perform(replaceText(purchaseDate), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.make)).perform(typeText(make), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.model)).perform(typeText(model), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.estimatedValue)).perform(typeText(""+ estimatedValue), pressKey(KeyEvent.KEYCODE_TAB),
+                pressKey(KeyEvent.KEYCODE_TAB), pressKey(KeyEvent.KEYCODE_TAB), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.add_tag_button)).perform(click());
+        onView(withId(R.id.editTextTagInput)).perform(typeText(tag), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.buttonCreate)).perform(click());
+        onView(withId(R.id.editTextTagInput)).perform(typeText("test"), pressKey(KeyEvent.KEYCODE_TAB));
+        onView(withId(R.id.buttonCreate)).perform(click());
+        onView(withId(R.id.buttonDone)).perform(click());
+        onView(withId(R.id.btn_add_item)).perform(click());
+    }
+
+    @Before
+    public void signIn(){
+        final CountDownLatch latch = new CountDownLatch(1);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword("testuser@gmail.com", "password")
+                .addOnCompleteListener(ApplicationProvider.getApplicationContext().getMainExecutor(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            latch.countDown();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                        }
+                    }
+                });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public class RecyclerViewItemCountAssertion implements ViewAssertion {
+        private final int expectedCount;
+
+        public RecyclerViewItemCountAssertion(int expectedCount) {
+            this.expectedCount = expectedCount;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            if (noViewFoundException != null) {
+                throw noViewFoundException;
+            }
+
+            RecyclerView recyclerView = (RecyclerView) view;
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+            assertThat(adapter.getItemCount(), is(expectedCount));
+        }
+    }
+    private void deleteItem() {
+        onView(withId(R.id.item_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.btnDelete)).perform(click());
+    }
 }
